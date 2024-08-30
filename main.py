6ef28964bf7e909fa24e8c0812861c453f0614ac
gewdvibes <gewdvibes@proton.me>
2024-08-30 04:23:34 -0400

    made the display work and quitting works

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ def main():
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    running = True
+
+    # pygame setup
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill("black")
+
+        pygame.display.flip()
+
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
